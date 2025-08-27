9bdd91073bd13738e775a0112b7b64b854ef9ba3
Vardaan <86072013+v-161@users.noreply.github.com>
2025-08-28 00:31:59 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base URL comes from environment variable (Netlify sets REACT_APP_API_BASE)
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:10000";
 
 const api = axios.create({
   baseURL: API_BASE,
